0da14915af2319555777c0a62cb91c90e0544018
Tania <taniakononenko23@gmail.com>
2023-03-06 14:34:58 +0200

    git init files

---
+++ task2/calculator.js
@@ -0,0 +1,9 @@
+export const getSquaredArray = (arr => {
+    return arr.map(num => num * num);
+});
+
+export const getOddNumbers = (arr => {
+    return arr.filter(num => (num % 2 === 1));
+});
+
+export default (a, b) => a + b;
